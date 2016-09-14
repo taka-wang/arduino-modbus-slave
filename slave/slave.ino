fb1fa5c8583739c0f126a57392162b523c00ba39
Taka Wang <wang.chia.ming@gmail.com>
2016-09-14 13:25:05 +0800

    add another led

--- slave/slave.ino
+++ slave/slave.ino
@@ -10,9 +10,11 @@
 #include "ModbusIP.h"
 
 //Modbus Registers Offsets (0-9999)
-const int LAMP1_COIL = 100; 
+const int LAMP1_COIL = 100;
+const int LAMP2_COIL = 200; 
 //Used Pins
-const int ledPin = 9;
+const int ledPin1 = 8;
+const int ledPin2 = 9;
 
 //ModbusIP object
 ModbusIP mb;
@@ -21,13 +23,15 @@ void setup() {
     // The media access control (ethernet hardware) address for the shield
     byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  
     // The IP address for the shield
-    byte ip[] = { 192, 168, 1, 120 };   
+    byte ip[] = { 192, 168, 0, 200 };   
     //Config Modbus IP 
     mb.config(mac, ip);
     //Set ledPin mode
-    pinMode(ledPin, OUTPUT);
+    pinMode(ledPin1, OUTPUT);
+    pinMode(ledPin2, OUTPUT);
     // Add LAMP1_COIL register - Use addCoil() for digital outputs
     mb.addCoil(LAMP1_COIL);
+    mb.addCoil(LAMP2_COIL);
 
     mb.addCoil(101, false);
     mb.addCoil(102, false);
@@ -41,5 +45,6 @@ void loop() {
    mb.task();
    
    //Attach ledPin to LAMP1_COIL register     
-   digitalWrite(ledPin, mb.Coil(LAMP1_COIL));
+   digitalWrite(ledPin1, mb.Coil(LAMP1_COIL));
+   digitalWrite(ledPin2, mb.Coil(LAMP2_COIL));
 }
